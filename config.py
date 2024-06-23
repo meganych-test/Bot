e85ae8f46acbae44f04b663947b2bec2800d083f
meganych-test <110050549+meganych-test@users.noreply.github.com>
2024-06-23 14:30:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 BOT_TOKEN = '7205345847:AAEVHooV_vDBj45bOLm6dAMf3PAsaxvAUek'
 WEBAPP_URL = 'https://meganych-test.github.io/Circle-Cliker-1/'
 BUTTON_TEXT = 'CLICK THIS!'
-GREETING_MESSAGE = 'aiogram is a modern and fully asynchronous framework for \nTelegram Bot API written in Python 3.8+ using asyncio and aiohttp. Make your bots faster and more powerful!'
+GREETING_MESSAGE = 'Hello there! We are currently experiencing some technical difficulties with our website. We apologize for any inconvenience this may have caused you. We are doing everything we can to fix the issue right now.'
