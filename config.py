72efa87147c38f26bed160086d398af82d6f4857
meganych-test <110050549+meganych-test@users.noreply.github.com>
2024-06-30 19:48:21 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-BOT_TOKEN = '7205345847:AAEVHooV_vDBj45bOLm6dAMf3PAsaxvAUek'
-WEBAPP_URL = 'https://meganych-test.github.io/Circle-Cliker-1/'
-BUTTON_TEXT = 'CLICK THIS!'
-GREETING_MESSAGE = 'Lets practice everything we tried so far by coding an Echo Bot. \n\nIts functionality will be rather simple: every text message it receives will be sent right back ...'
-
-# New configurations for the second button
-SECOND_BUTTON_TEXT = 'Meganych Channel'
-SECOND_BUTTON_URL = 'https://t.me/meganych'
