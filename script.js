4d51dfd8b94d815c741a81720de9c5136fde2814
meganych-test <110050549+meganych-test@users.noreply.github.com>
2024-07-01 10:19:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,15 @@
         // Additional initialization code can go here
     };
 </script>
+<script>
+mergeInto(LibraryManager.library, {
+  TryVibrate: function(duration) {
+    if ("vibrate" in navigator) {
+      navigator.vibrate(duration);
+      console.log("Vibration requested for " + duration + " milliseconds");
+    } else {
+      console.log("Vibration not supported in this browser");
+    }
+  }
+});
+</script>
